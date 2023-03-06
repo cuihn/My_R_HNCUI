f65b06b235c1029fcd6a21b1454dca9b51add182
Haining <cuihn1989@gmail.com>
2023-03-06 12:53:53 -0500

    deleted old code without looping and some stats tried on 20230305, ICC, plotting data

--- RatingScale_G1.R
+++ RatingScale_G1.R
@@ -19,6 +19,10 @@ library(dplyr)
 # install.packages("magrittr") 
 library(magrittr)
 library(tidyr)
+# install.packages("labeling")
+library(labeling)
+# install.packages("ICC")
+library(irr)
 
 for (i in 1:4) {
   file_name <- paste0("G", i, "_Q1.txt")
@@ -51,131 +55,14 @@ transposed_data_list <- lapply(1:4, function(i) {
 })
 
 # merge the data frames with an index column
-merged_data_Q1 <- bind_rows(transposed_data_list, .id = "Domain")
+merged_data_Q1 <- bind_rows(transposed_data_list)
 
-# ggplot to plot rating patterns,conduct basic stats, ICC analysis
+# Summary statistics
 
+summary_df <- merged_data_Q1 %>%
+  group_by(Form) %>%
+  summarise(Mean = mean(Rating), Median = median(Rating), SD = sd(Rating))
 
-
-
-
-# ### import G1_Q1 data
-# 
-# G1_Q1_raw_data <- read.delim('/Users/hainingcui/Dropbox/Trying/G1_Q1.txt')
-# G1_Q1_new <- dplyr::select(G1_raw_data, 1, 10, 11, 20:ncol(G1_raw_data), 
-#                         -starts_with("X"))
-# names(G1_Q1_new)[1] <- "ID"
-# 
-# #transpose rows to columns/variables
-# G1_Q1_transposed <- G1_Q1_new %>%
-#   gather(key = "SentenceType", value = "Rating", -ID, -Age, -Gender) %>%
-#   mutate(Domain = ifelse(grepl("(M|D)", SentenceType) & grepl("M", SentenceType), 
-#                          "M", "D")) %>%
-#   mutate(Action = ifelse(grepl("(I|A)", SentenceType) & grepl("I", SentenceType), 
-#                          "I", "A")) %>%
-#   mutate(Form = ifelse(grepl("(R|W)", SentenceType) & grepl("R", SentenceType), 
-#                          "R", "W")) %>%
-#   drop_na()
-# 
-# print(G1_Q1_transposed)
-# 
-# ### import G1_Q2 data
-# 
-# G1_Q2_raw_data <- read.delim('/Users/hainingcui/Dropbox/Trying/G1_Q2.txt') 
-# names(G1_Q2_raw_data)[1] <- "ID"
-# 
-# #transpose rows to columns/variables
-# G1_Q2_transposed <- G1_Q2_raw_data  %>%
-#   gather(key = "SentenceType", value = "Rating", -ID, -Age, -Gender) %>%
-#   mutate(Domain = ifelse(grepl("(M|D)", SentenceType) & grepl("M", SentenceType), 
-#                          "M", "D")) %>%
-#   mutate(Action = ifelse(grepl("(I|A)", SentenceType) & grepl("I", SentenceType), 
-#                          "I", "A")) %>%
-#   mutate(Form = ifelse(grepl("(R|W)", SentenceType) & grepl("R", SentenceType), 
-#                        "R", "W")) %>%
-#   drop_na()
-# 
-# print(G1_Q2_transposed)
-# 
-# 
-# ### import G2_Q1 data
-# 
-# G2_raw_data <- read.delim('/Users/hainingcui/Dropbox/Trying/G2_Q1.txt')
-# G2_new <- dplyr::select(G2_raw_data, 1, 11, 12, 21:ncol(G2_raw_data),
-#                         -starts_with("X")) 
-# names(G2_new)[1] <- "ID"
-# 
-# # transpose rows to columns/variables
-# G2_transposed <- G2_new %>%
-#   gather(key = "SentenceType", value = "Rating", -ID, -Age, -Gender) %>%
-#   drop_na()
-# 
-# print(G2_transposed)
-# 
-# ### import G2_Q2 data
-# 
-# G2_Q2_raw_data <- read.delim('/Users/hainingcui/Dropbox/Trying/G2_Q2.txt') 
-# names(G2_Q2_raw_data)[1] <- "ID"
-# 
-# #transpose rows to columns/variables
-# G2_Q2_transposed <- G2_Q2_raw_data  %>%
-#   gather(key = "SentenceType", value = "Rating", -ID) %>%
-#   drop_na()
-# 
-# print(G2_Q2_transposed)
-# 
-# 
-# ### import G3_Q1 data
-# G3_raw_data <- read.delim('/Users/hainingcui/Dropbox/Trying/G3_Q1.txt')
-# G3_new <- dplyr::select(G3_raw_data, 1, 10, 11, 20:ncol(G3_raw_data), 
-#                         -starts_with("X")) 
-# names(G3_new)[1] <- "ID"
-# 
-# #transpose rows to columns/variables （wide format to long format)
-# G3_transposed <- G3_new %>%
-#   gather(key = "SentenceType", value = "Rating", -ID, -Age, -Gender) %>%
-#   drop_na()
-# 
-# print(G3_transposed)
-# 
-# ### import G3_Q2 data
-# 
-# G3_Q2_raw_data <- read.delim('/Users/hainingcui/Dropbox/Trying/G3_Q2.txt') 
-# names(G3_Q2_raw_data)[1] <- "ID"
-# 
-# #transpose rows to columns/variables
-# G3_Q2_transposed <- G3_Q2_raw_data  %>%
-#   gather(key = "SentenceType", value = "Rating", -ID) %>%
-#   drop_na()
-# 
-# print(G3_Q2_transposed)
-# 
-# 
-# ### import G4_Q1 data 
-# G4_raw_data <- read.delim('/Users/hainingcui/Dropbox/Trying/G4_Q1.txt')
-# G4_new <- dplyr::select(G4_raw_data, 1, 11, 12, 21:ncol(G4_raw_data), 
-#                         -starts_with("X")) 
-# names(G4_new)[1] <- "ID"
-# 
-# # transpose rows to columns/variables （wide format to long format)
-# G4_transposed <- G4_new %>%
-#   gather(key = "SentenceType", value = "Rating", -Gender, -ID, -Age, ) %>%
-#   drop_na()
-# 
-# print(G4_transposed)
-# 
-# ### import G4_Q2 data
-# 
-# G4_Q2_raw_data <- read.delim('/Users/hainingcui/Dropbox/Trying/G4_Q2.txt') 
-# names(G4_Q2_raw_data)[1] <- "ID"
-# 
-# #transpose rows to columns/variables
-# G4_Q2_transposed <- G4_Q2_raw_data  %>%
-#   gather(key = "SentenceType", value = "Rating", -ID) %>%
-#   drop_na()
-# 
-# print(G4_Q2_transposed)
-# 
-# ### merge all the tansposed datafames into one with sentenceType as index
-# 
-# merged_all <- merge(G1_transposed,G1_Q2_transposed, merged__all)
+# subset the data for the two groups
+Action_A <- merged_data_Q1$Rating[merged_data_Q1$Action == "A"]
+Action_I <- merged_data_Q1$Rating[merged_data_Q1$Action == "I"]
