79c33d7ac2915802c532c73c4be810df76d5f5d0
Haining <cuihn1989@gmail.com>
2023-03-06 14:51:06 -0500

    changed example_run successfully

--- Fleiss' kappa test_example.R
+++ Fleiss' kappa test_example.R
@@ -19,3 +19,27 @@
 # library(psych)
 # reliability <- alpha(ratings)
 # reliability$total
+
+library(irr)
+
+# Create example data
+set.seed(123)
+ratings <- data.frame(replicate(18, sample(1:5, 260, replace = TRUE)))
+
+# Calculate ICC
+icc <- icc(ratings, model = "twoway", type = "agreement", unit = "single")
+
+# Identify items with low ICC
+low_icc_items <- which(icc$value < 0.3)
+
+# Create new dataset with low-ICC items removed
+new_ratings <- ratings[,-low_icc_items]
+
+# Print summary statistics for new dataset
+summary(new_ratings)
+
+# print
+print(low_icc_items )
+
+
+
