9c7576c7123726692da3ded1945163a669f6e7da
Haining <cuihn1989@gmail.com>
2023-03-09 12:14:43 -0500

    changed location

--- openAI_intergrate.R
+++
@@ -1,7 +0,0 @@
-install.packages("reticulate")
-library(reticulate)
-use_python("/Users/hainingcui/opt/anaconda3/myenv/bin/python")
-py_install("openai")
-openai <- import("openai")
-openai$api_key <- "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"
-
